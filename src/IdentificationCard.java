8e0318179998970a750c72b4cd0a56f2ccde4cd1
zyle-geralde <zylgerdlp19@gmail.com>
2023-12-06 14:19:50 +0800

    added card

---
+++ src/IdentificationCard.java
@@ -0,0 +1,2 @@
+public class IdentificationCard extends Card{
+}
--- src/MultipleChoiceCard.java
+++ src/MultipleChoiceCard.java
@@ -2,7 +2,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MultipleChoiceCard extends Card{
-    ArrayList<String> choices = new ArrayList<>();
+    ArrayList<String> choices;
 
+    public MultipleChoiceCard(){
+        choices = new ArrayList<>();
+    }
+    public void getChoice(){
+        //do something
+    }
 
 }
+
