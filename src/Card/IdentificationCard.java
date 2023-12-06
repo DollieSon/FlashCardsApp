3f9d2d53c559ca902947ef4b934973328428cabc
DollieSon <62559681+DollieSon@users.noreply.github.com>
2023-12-06 16:01:58 +0800

    TT2

--- src/Card/IdentificationCard.java
+++ src/Card/IdentificationCard.java
@@ -4,12 +4,19 @@ public class IdentificationCard extends Card {
     private String answer;
 
     @Override
+    public boolean checkAnswer() {
+        return false;
+    }
+
+    @Override
     public void getAnswer() {
         // IMPLEMENT
     }
 
     public void setAnswer(String Answer) { this.answer = Answer; }
 
-    @Override
-    public boolean checkAnswer() { return false; }
+
+    public boolean checkAnswer(String Answer) {
+        return answer.equals(Answer);
+    }
 }
--- src/SampleUi.java
+++ src/SampleUi.java
@@ -1,3 +1,5 @@
+import Card.IdentificationCard;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,9 +8,13 @@ public class SampleUi extends JFrame{
     private JPanel jpanel;
     private JTextField tfAnswer;
     private JButton bCheck;
-
+    IdentificationCard idem;
 
     public SampleUi(){
+        idem = new IdentificationCard();
+        idem.setQuestion("1+1");
+        idem.setAnswer("2");
+
         bCheck.setText("Check Answer");
         bCheck.addActionListener(new ActionListener() {
             @Override
@@ -17,6 +23,9 @@ public class SampleUi extends JFrame{
                 if(bb.equals("")){
                     JOptionPane.showConfirmDialog(null,"Enter a value");
                 }
+                else if(idem.checkAnswer(bb) == true){
+                    JOptionPane.showConfirmDialog(null,"Correct");
+                }
                 else{
                     JOptionPane.showConfirmDialog(null,"Good");
                 }
@@ -24,6 +33,7 @@ public class SampleUi extends JFrame{
         });
     }
     public static void main(String [] args){
+
         SampleUi m = new SampleUi();
         m.setContentPane(m.jpanel);
         m.setSize(500, 300);
