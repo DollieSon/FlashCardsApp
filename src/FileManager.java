bbf876b4580c9f70b6034bed5465a70744eb3d94
DollieSon <62559681+DollieSon@users.noreply.github.com>
2023-12-06 13:13:30 +0800

    Added My Thing

--- src/FileManager.java
+++ src/FileManager.java
@@ -4,17 +4,31 @@ public class FileManager {
     private BufferedReader Reader;
     private BufferedWriter Writer;
 
-    public FileManager(String Location) throws FileNotFoundException,IOException{
-        try{
-            Reader = new BufferedReader(new FileReader(Location));
-            Writer = new BufferedWriter(new FileWriter(Location));
-        }catch(FileNotFoundException e){
-            throw new FileNotFoundException("Error In Buffered Reader");
-        }catch (IOException e){
-            throw new IOException("Error in Writer");
-        }
+    private String Location;
+
+    public FileManager(String Location) throws FileNotFoundException{
+        this.Location = Location;
         System.out.println("FM created");
     }
 
+    public String GetString() throws IOException {
+        Reader = new BufferedReader( new FileReader(Location));
+        try{
+            return Reader.readLine();
+        }catch (IOException e){
+            throw new IOException("Error REading Line");
+        }
+    }
+
+    public Boolean WriteLine(String line) throws IOException{
+        Writer = new BufferedWriter(new FileWriter(Location));
+        try{
+            Writer.write(line);
+            Writer.flush();
+        }catch (IOException e){
+            throw new IOException("Error Writing to File");
+        }
+        return true;
+    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,18 @@
+import java.io.IOException;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        FileManager File1 = FileHandlerFactory.getFileManager("src\\Quiz1.txt");
         FileManager File2 = FileHandlerFactory.getFileManager("src\\Sample.txt");
-        FileManager File3 = FileHandlerFactory.getFileManager("src\\Quiz1.txt");
-        System.out.println( File1);
         System.out.println( File2);
-        System.out.println( File3);
+        try{
+            System.out.println(File2.GetString());
+            File2.WriteLine("Gibbie");
+            System.out.println(File2.GetString());
+        }catch (IOException e){
 
+        }
         System.out.println("HEllo world");
     }
 }
