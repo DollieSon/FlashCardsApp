e2e1922426067be8a82bf1817ab64a5a04b20f16
DollieSon <62559681+DollieSon@users.noreply.github.com>
2023-12-06 13:57:32 +0800

    Implemented Card and Quiz

---
+++ src/Quiz.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Quiz {
+    private String QuizName;
+    private List<Card> Questions;
+    private String Author;
+
+    public void setName(String Name){
+        QuizName = Name;
+    }
+
+    Quiz addCard(Card card){
+
+    }
+
+}
