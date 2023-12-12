2b9d9f2bc843ccc451e309f4e97b7fb8841c12e6
DollieSon <62559681+DollieSon@users.noreply.github.com>
2023-12-12 22:23:02 +0800

    Added Serialization to Saving

--- src/QuizPackage/SerializationUtil.java
+++ src/QuizPackage/SerializationUtil.java
@@ -7,6 +7,7 @@ public class SerializationUtil{
     // deserialize to Object from given file
     public static Object deserialize(String fileName) throws IOException,
             ClassNotFoundException {
+
         FileInputStream fis = new FileInputStream(fileName);
         ObjectInputStream ois = new ObjectInputStream(fis);
         Object obj = ois.readObject();
@@ -17,6 +18,8 @@ public class SerializationUtil{
     // serialize the given object and save it to file
     public static void serialize(Object obj, String fileName)
             throws IOException {
+        System.out.println(fileName);
+        System.out.println(((Quiz)obj).getQuizName());
         FileOutputStream fos = new FileOutputStream(fileName);
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(obj);
